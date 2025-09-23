0b155256894fb60e9f739b60b344581bbb95d085
jeevan4444 <jeevanpandey4444@gmail.com>
2025-09-23 18:49:22 +0545

    Add files via upload

---
+++ function.js
@@ -0,0 +1,39 @@
+// 1. Write a function printTriangle(n) that prints a triangle pattern of * with n rows. 
+// function printTriangle(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log('*'.repeat(i));
+//   }
+// }
+// printTriangle(3)
+
+
+
+// 2. Write a function printReverseTriangle(n) that prints an inverted triangle of *. 
+// function printReverseTriangle(n) {
+//   for (let i = n; i >= 1; i--) {
+//     console.log('*'.repeat(i));
+//   }
+// }
+// printReverseTriangle(5)
+
+
+
+
+
+
+// 4. Write a function printFibonacciSeries(n) that prints the first n Fibonacci numbers. 
+// function printFibonacciSeries(n) {
+//   let a = 0, b = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     console.log(a);
+//     let next = a + b;
+//     a = b;
+//     b = next;
+//   }
+// }
+// printFibonacciSeries(5)
+
+
+
+
