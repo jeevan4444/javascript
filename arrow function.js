5e59fec6aee3a7149b86727ae0cb7ecfff07ea52
jeevan4444 <jeevanpandey4444@gmail.com>
2025-11-03 14:46:15 +0545

    Add files via upload

---
+++ arrow function.js
@@ -0,0 +1,102 @@
+// 1.// Write an arrow function that returns the sum of two numbers
+
+// const add=(a,b)=>{
+//     return a+b
+// }
+// console.log(add(2,5))
+
+// 2.Write an arrow function that returns the square of a number.
+
+// const square=(a)=>{
+//     return a*a
+// }
+
+// console.log(square(4))
+
+// 3.Write an arrow function that checks if a number is even or odd
+
+// const check=(a)=>{
+//     if(a%2==0){
+//         return ("even")
+//     }else{
+//        return ("odd")
+//     }
+// }
+// console.log(check(4))
+// console.log(check(5))
+
+// 4.. Write an arrow function that returns the length of a given string.
+
+// const length=(a)=>{
+
+//     return a.length
+// }
+
+// console.log(length('hello'))
+
+// 5Write an arrow function that converts a string to uppercase
+
+// const upper=(a)=>{
+//     return a.toUpperCase()
+// }
+
+// console.log(upper("ram")) 
+
+// 6.Write an arrow function that concatenates two strings.
+
+// const strAdd=(a,b)=>{
+//     return a.concat(" ",b)
+// }
+
+// console.log(strAdd('ram','shyam'))
+
+
+
+// 8. Write an arrow function that checks if a number is positive, negative, or zero.
+
+// const checkNum = (n)=>{
+//     if (n > 0) {
+//         return "positive"
+//     } else if (n < 0) {
+//         return "negative"
+//     } else {
+//         return "zero"
+//     }
+// };
+
+// console.log(checkNum(5))  
+// console.log(checkNum(-3))  
+// console.log(checkNum(0))  
+
+
+// 9. Write an arrow function that takes an array of numbers and returns the first element.
+
+// const firstElement = (arr) => {
+//     return arr[0]
+// };
+
+// const firstarray=[1,2,3]
+// console.log(firstElement(firstarray))
+
+
+// 10. Write an arrow function that returns the last character of a given string.
+
+// const lastChar = (str) => {
+//     return str[str.length - 1]
+// };
+
+// console.log(lastChar('ram'))
+
+
+// 11. Write an arrow function that takes an array and returns the sum of its elements.
+
+// const sumArray = arr => {
+//     let sum = 0
+//     for (let i=0; i<arr.length; i++) {  
+//         sum += arr[i]
+//     }
+//     return sum
+// }
+
+// const number=[1,2,3,4,5]
+// console.log(sumArray(number))
