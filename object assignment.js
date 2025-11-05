7d014a5c1381a1a82d25b01e52e832bd2381046d
jeevan4444 <jeevanpandey4444@gmail.com>
2025-11-05 20:03:31 +0545

    Add files via upload

---
+++ object assignment.js
@@ -0,0 +1,106 @@
+// 1.) Create an object `student` with properties `name`, `age`, and `grade`. Assign values to each 
+// property. 
+
+// const student = {
+//   name: "Jeevan",
+//   age: 22,
+//   grade: "A"
+// };
+
+// 2. Given an object `car` with properties `make`, `model`, and `year`, write a code to log the 
+// `make` and `model` of the car. 
+
+// const car = {
+//   make: "Toyota",
+//   model: "Corolla",
+//   year: 2020
+// };
+
+// console.log("Make:", car.make);
+// console.log("Model:", car.model);
+
+// 3.. Change the `grade` property of the `student` object from "A" to "A+". 
+
+// const student = {
+//   name: "Jeevan",
+//   age: 22,
+//   grade: "A"
+// };
+
+
+// student.grade = "A+";
+
+// console.log(student);
+
+// 4.Add a new property `address` to the `student` object with the value "123 Main St". 
+
+// const student = {
+//   name: "Jeevan Pandey",
+//   age: 22,
+//   grade: "A+"
+// };
+
+// student.address = "123 Main St";
+
+// console.log(student);
+
+
+// 6. Delete the `address` property from the `student` object. 
+
+
+// const student = {
+//   name: "Jeevan Pandey",
+//   age: 22,
+//   grade: "A+",
+//   address: "123 Main St"
+// };
+
+
+// delete student.address;
+
+// console.log(student);
+
+
+// 16. Create a nested object  with properties , , and  where  is another object with properties , , and .
+
+
+// const company = {
+//   name: "sipalaya",
+//   location: "Bharatpur",
+//   departments: {
+    
+//     Training: "Software Development",
+
+//   }
+// };
+
+// 21. 	Write an if-else statement that checks if the  property of a  object is greater than 18 and logs "Adult" or "Minor."
+// const person = {
+//   name: "jeevan",
+//   age: 20
+// };
+
+// if (person.age > 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+// 9. 	Create an array of objects representing books, where each object has properties , , and .
+// const books = [
+//   {
+//     title: "Clean Code",
+//     author: "Robert C. Martin",
+//     year: 2008
+//   },
+//   {
+//     title: "You Don't Know JS",
+//     author: "Kyle Simpson",
+//     year: 2015
+//   },
+//   {
+//     title: "Eloquent JavaScript",
+//     author: "Marijn Haverbeke",
+//     year: 2018
+//   }
+// ];
